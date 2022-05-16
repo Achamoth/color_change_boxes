84df60b7d486b0f52158b2846a53d4b15c37ff3e
Achamoth <21521274@student.uwa.edu.au>
2022-05-16 21:12:31 +0800

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+
+interface BoxProps {
+  text: string;
+  color: string;
+  width: number;
+  length: number;
+}
+
+function Box({ text, color, width, length }: BoxProps): React.ReactElement {
+  return;
+}
+
+export default function App() {
+  return <div />;
+}
